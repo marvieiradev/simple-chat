3efd70cbe61a192f7ee34794f834f9321b7c3ca9
Marcos Vieira <marcosvieira482@gmail.com>
2024-05-09 19:32:24 -0300

    feat: create function to send and receive messages, and identify user

--- web/js/script.js
+++ web/js/script.js
@@ -27,15 +27,41 @@ const createMessageSelfElement = (content) => {
     return div;
 }
 
+const createMessageOtherElement = (content, sender, senderColor) => {
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+    div.classList.add("message__other");
+    span.classList.add("message__sender");
+    span.style.color = senderColor;
+
+    div.appendChild(span);
+
+    span.innerHTML = sender;
+    div.innerHTML += content;
+    return div;
+}
+
 const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
 }
 
+const scrollScreen = () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
+}
+
 const processMessage = ({ data }) => {
     const { userId, userName, userColor, content } = JSON.parse(data);
-    const element = createMessageSelfElement(content);
-    chatMessages.appendChild(element);
+
+    const message = userId == user.id
+        ? createMessageSelfElement(content)
+        : createMessageOtherElement(content, userName, userColor);
+
+    chatMessages.appendChild(message);
+    scrollScreen();
 }
 const handleLogin = (event) => {
     event.preventDefault();
