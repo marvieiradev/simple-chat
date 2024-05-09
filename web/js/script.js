e0a2627211b8703d8617e5b24744a8c24457e999
Marcos Vieira <marcosvieira482@gmail.com>
2024-05-09 18:51:34 -0300

    feat: add connection to server

---
+++ web/js/script.js
@@ -0,0 +1,43 @@
+const login = document.querySelector(".login");
+const loginForm = login.querySelector(".login__form");
+const loginInput = login.querySelector(".login__input");
+
+const chat = document.querySelector(".chat");
+const chatForm = chat.querySelector(".chat__form");
+const chatInput = chat.querySelector(".chat__input");
+
+const colors = [
+    "cadetblue",
+    "darkgoldenrod",
+    "cornflowerblue",
+    "darkkhaki",
+    "hotpink",
+    "gold"
+];
+
+const user = { id: "", name: "", color: "" };
+
+let websocket;
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+const processMessage = ({ data }) => {
+    console.log(data);
+}
+const handleLogin = (event) => {
+    event.preventDefault();
+
+    user.id = crypto.randomUUID();
+    user.name = loginInput.value;
+    user.color = getRandomColor();
+
+    login.style.display = "none";
+    chat.style.display = "flex";
+
+    websocket = new WebSocket("ws://localhost:8080");
+    websocket.onmessage = processMessage;
+}
+loginForm.addEventListener("submit", handleLogin);
